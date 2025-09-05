a5342687fe79f85e2ac3623492fc146394fbd572
Jeff Squyres <jeff@squyres.com>
2025-09-05 09:46:33 -0400

    calculator.py: convert to use logging
    Instead of printing, using the Python logging module for all output.
    
    Signed-off-by: Jeff Squyres <jeff@squyres.com>

--- calculator.py
+++ calculator.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import sys
+import logging
+
 from random import seed, randrange
 
 def add():
@@ -9,19 +12,22 @@ def subtract():
     return randrange(1000) - randrange(1000)
 
 def main():
+    logging.basicConfig(level=logging.INFO,
+                        stream=sys.stdout,
+                        format='%(levelname)s: %(message)s')
     seed(0)
 
-    print("Welcome to the [not-so] random number calculator!")
+    logging.info("Welcome to the [not-so] random number calculator!")
 
     op = randrange(2)
     if op == 0:
-        print("Adding!")
+        logging.debug("Adding!")
         result = add()
     elif op == 1:
-        print("Subtracting!")
+        logging.debug("Subtracting!")
         result = subtract()
 
-    print(f"The result is {result}")
+    logging.info(f"The result is {result}")
 
 if __name__ == "__main__":
     main()
